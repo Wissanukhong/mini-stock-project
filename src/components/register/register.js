c93b67638d35085fff18fcd5214e2d2448d4a90a
Wissanukhong <wissanu.khong@gmail.com>
2020-03-22 22:18:30 +0700

    event onChange  username and  password

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 
 class Register extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
   render() {
     return (
       <div className="login-box">
@@ -18,6 +27,8 @@ class Register extends Component {
           <form>
             <div className="form-group has-feedback">
               <input
+                onChange={(e)=>this.setState({username: e.target.value})}
+                name="username"
                 type="email"
                 className="form-control"
                 placeholder="Email"
@@ -26,13 +37,19 @@ class Register extends Component {
             </div>
             <div className="form-group has-feedback">
               <input
+                onChange={(e)=>this.setState({password: e.target.value})}
+                name="password"
                 type="password"
                 className="form-control"
                 placeholder="Password"
               />
+              console.log(name)
               <span className="glyphicon glyphicon-lock form-control-feedback" />
             </div>
 
+            {/* คำสั่งบรรทัดนี้ จะใช้เมื่อเราต้องการเช็คว่า สิ่งที่ User พิมพ์เข้ามา ถูกจัดเก็บเข้าไปที่ state หรือไม่ โดยการแสดงที่หน้าจอ */}
+            <span>#Debug: {JSON.stringify(this.state)}</span>
+            
             {/* Register */}
             <div className="row">
               <div className="col-xs-12">
@@ -50,7 +67,7 @@ class Register extends Component {
                 <button
                   onClick={e => {
                     e.preventDefault();
-                    this.props.history.goBack()
+                    this.props.history.goBack();
                   }}
                   type="submit"
                   style={{ marginTop: 8 }}
