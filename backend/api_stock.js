0034fa68909d6b1efc266faed169437a308ad982
Wissanukhong <wissanu.khong@gmail.com>
2020-03-23 13:44:46 +0700

    postman api

---
+++ backend/api_stock.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router()
+
+router.get("/product", (req, res)=>{
+    res.json({result: "product"})
+})
+
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express()
+const bodyParser = require('body-parser')
 
-app.get("/register", (req, res)=>{
-    res.end("Hey Register")
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.get("/login", (req, res)=>{
-    res.json({result: "OK"})
-})
+app.use("/api/v2/authen/", require("./api_authen.js"))
+app.use("/api/v2/stock/", require("./api_stock.js"))
 
 app.listen(8085, ()=>{
     console.log("Bcakend is running....")
