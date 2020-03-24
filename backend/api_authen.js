fc5093712cb1c4b213d853e45f674a255bbdb5d0
Wissanukhong <wissanu.khong@gmail.com>
2020-03-24 21:30:52 +0700

    login

--- backend/api_authen.js
+++ backend/api_authen.js
@@ -5,16 +5,32 @@ const bcrypt = require("bcryptjs");
 const constants = require("./constant");
 
 //Login
-router.post("/login", (req, res)=>{
-    res.json({result: "login"})
-})
+router.post("/login", async(req, res) => {
+    const { username, password } = req.body;
 
-router.post("/register",async (req, res)=>{
+    let result = await user.findOne({ where: { username: username } });
+    if (result != null) {
+      if (bcrypt.compareSync(password, result.password)) {
+        res.json({
+          result: constants.kResultOk,
+          message: JSON.stringify(result)
+        });
+      } else {
+        res.json({ result: constants.kResultNok, message: "Incorrect password" });
+      }
+    } else {
+      res.json({ result: constants.kResultNok, message: "Incorrect username" });
+    }
+});
 
+router.post("/register", async (req, res) => {
+  try {
     req.body.password = bcrypt.hashSync(req.body.password, 8);
-    let result = await user.create(req.body)
-
-    res.json({result: constants.kResultOk, message: JSON.stringify(result)})
-})
+    let result = await user.create(req.body);
+    res.json({ result: constants.kResultOk, message: JSON.stringify(result) });
+  } catch (error) {
+    res.json({ result: constants.kResultNok, message: JSON.stringify(error) });
+  }
+});
 
 module.exports = router;
