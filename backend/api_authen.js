66b1ceaaab452df147e42a37f04f32114abac7df
Wissanukhong <wissanu.khong@gmail.com>
2020-03-24 16:26:15 +0700

    data base and DBbrowwser

--- backend/api_authen.js
+++ backend/api_authen.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router()
+const user = require("./models/user");
 
 router.post("/login", (req, res)=>{
     res.json({result: "login"})
 })
 
-router.post("/register", (req, res)=>{
+router.post("/register",async (req, res)=>{
 
     const {username, password} =req.body
+    await user.create(req.body)
 
     res.json({result: "OK", username, password})
 })
