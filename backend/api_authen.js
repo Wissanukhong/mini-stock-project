1e2049b468b393ac66a9f1f409bfbc4d9a1db7da
Wissanukhong <wissanu.khong@gmail.com>
2020-03-24 17:21:35 +0700

    DB

--- backend/api_authen.js
+++ backend/api_authen.js
@@ -1,17 +1,20 @@
 const express = require("express");
-const router = express.Router()
+const router = express.Router();
 const user = require("./models/user");
+const bcrypt = require("bcryptjs");
+const constants = require("./constant");
 
+//Login
 router.post("/login", (req, res)=>{
     res.json({result: "login"})
 })
 
 router.post("/register",async (req, res)=>{
 
-    const {username, password} =req.body
-    await user.create(req.body)
+    req.body.password = bcrypt.hashSync(req.body.password, 8);
+    let result = await user.create(req.body)
 
-    res.json({result: "OK", username, password})
+    res.json({result: constants.kResultOk, message: JSON.stringify(result)})
 })
 
 module.exports = router;
