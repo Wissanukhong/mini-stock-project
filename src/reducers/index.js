c8d6d783b2e9a35ede15f4c350485a7a3477504a
Wissanukhong <wissanu.khong@gmail.com>
2020-03-25 22:57:30 +0700

    git refactoring code

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,13 @@
 import { combineReducers } from "redux";
 import registerReducer from './register.reducer'
-import loginReducer from './login.reducer'
-import appReducer from "./app.reducer";
-import stockReducer from "./stock.reducer";
+// import loginReducer from './login.reducer'
+// import appReducer from "./app.reducer";
+// import stockReducer from "./stock.reducer";
 
 
 export default combineReducers({
     registerReducer, 
-    loginReducer,
-    appReducer,
-    stockReducer, })
+    // loginReducer,
+    // appReducer,
+    // stockReducer, 
+})
