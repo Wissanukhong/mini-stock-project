af5f359c10c588b18e4f0deaac4e885089c2fce3
Wissanukhong <wissanu.khong@gmail.com>
2020-04-02 11:35:47 +0700

    debuger

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
-import {httpClient} from './../../utils/HttpClient';
-import { server } from "../../constants";
-//ถ้ามีคำว่า Default  {ไม่ต้องมี} แต่ถ้าไม่เป็น Default จะต้องมี {ปีกกาด้วย}
+// import { httpClient } from "./../../utils/HttpClient";
+// import { server } from "../../constants";
+// ถ้ามีคำว่า Default  {ไม่ต้องมี} แต่ถ้าไม่เป็น Default จะต้องมี {ปีกกาด้วย}
+import { connect } from "react-redux";
+import { register } from "./../../actions/register.action";
 
 class Register extends Component {
   constructor(props) {
@@ -14,15 +16,15 @@ class Register extends Component {
   }
 
   // การประกาศตัวแปรให้กับ function โดยการใช้ arrow function
-  onClickRegister = () => {
-    // จะต้องทำการตรวจเช็คทุกครั้งว่า api ถูกยิงไปที่ postman ถูกต้องไหม ถ้าไม่ถูกต้อง ต้องไปเช็คที่ฝั่ง Node ก่อน ว่ามีความถูกต้องไหม ถ้าถูกต้องก็กลับมาเช็คที่ Axios อีกที ว่าถูกต้องไหม 
-    // Axios.post("http://localhost:8085/api/v2/authen/register", this.state).then(response=>{ 
-    //   alert(JSON.stringify(response.data))
-    // })
-    httpClient.post(server.REGISTER_URL, this.state).then(response=>{
-      alert(JSON.stringify(response.data))
-    })
-  }
+  // onClickRegister = () => {
+  // จะต้องทำการตรวจเช็คทุกครั้งว่า api ถูกยิงไปที่ postman ถูกต้องไหม ถ้าไม่ถูกต้อง ต้องไปเช็คที่ฝั่ง Node ก่อน ว่ามีความถูกต้องไหม ถ้าถูกต้องก็กลับมาเช็คที่ Axios อีกที ว่าถูกต้องไหม
+  // Axios.post("http://localhost:8085/api/v2/authen/register", this.state).then(response=>{
+  //   alert(JSON.stringify(response.data))
+  // })
+  //   httpClient.post(server.REGISTER_URL, this.state).then(response => {
+  //     alert(JSON.stringify(response.data));
+  //   });
+  // };
 
   render() {
     return (
@@ -41,7 +43,7 @@ class Register extends Component {
           <form>
             <div className="form-group has-feedback">
               <input
-                onChange={(e)=>this.setState({username: e.target.value})}
+                onChange={e => this.setState({ username: e.target.value })}
                 name="username"
                 type="email"
                 className="form-control"
@@ -51,7 +53,7 @@ class Register extends Component {
             </div>
             <div className="form-group has-feedback">
               <input
-                onChange={(e)=>this.setState({password: e.target.value})}
+                onChange={e => this.setState({ password: e.target.value })}
                 name="password"
                 type="password"
                 className="form-control"
@@ -61,12 +63,14 @@ class Register extends Component {
             </div>
 
             {/* คำสั่งบรรทัดนี้ จะใช้เมื่อเราต้องการเช็คว่า สิ่งที่ User พิมพ์เข้ามา ถูกจัดเก็บเข้าไปที่ state หรือไม่ โดยการแสดงที่หน้าจอ */}
-            
+
             {/* Register */}
             <div className="row">
               <div className="col-xs-12">
                 <button
-                  onClick={this.onClickRegister}
+                  onClick={() =>
+                    this.props.register(this.props.history, this.state)
+                  }
                   type="submit"
                   className="btn btn-primary btn-block btn-flat"
                 >
@@ -97,4 +101,11 @@ class Register extends Component {
   }
 }
 
-export default Register;
+//map state and props shurt rxmap
+const mapStateToProps = ({ registerReducer }) => ({ registerReducer });
+
+const mapDispatchToProps = {
+  register
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Register);
