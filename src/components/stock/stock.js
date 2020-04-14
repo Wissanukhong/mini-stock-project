eca04647e9b18f5e16849635f431c47894b93ecb
Wissanukhong <wissanu.khong@gmail.com>
2020-04-14 07:23:31 +0700

    real template

--- src/components/stock/stock.js
+++ src/components/stock/stock.js
@@ -2,14 +2,14 @@ import React, { Component } from "react";
 import * as actions from "./../../actions/stock.action";
 import { connect } from "react-redux";
 
-// import { imageUrl } from "./../../constants";
-// import { Link } from "react-router-dom";
+import { imageUrl } from "./../../constants";
+import { Link } from "react-router-dom";
 import _ from "lodash";
-// import Moment from "react-moment";
-// import NumberFormat from "react-number-format";
-// import "./stock.css";
-// import Swal from "sweetalert2";
-// import withReactContent from "sweetalert2-react-content";
+import Moment from "react-moment";
+import NumberFormat from "react-number-format";
+import "./stock.css";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 class Stock extends Component {
   componentDidMount() {
@@ -23,7 +23,7 @@ class Stock extends Component {
     { c1: "xxx", c2: "xxx", c3: "xxx", c4: "xxx", c5: "xxx" },
     { c1: "xxx", c2: "xxx", c3: "xxx", c4: "xxx", c5: "xxx" },
     { c1: "xxx", c2: "xxx", c3: "xxx", c4: "xxx", c5: "xxx" },
-    { c1: "xxx", c2: "xxx", c3: "xxx", c4: "xxx", c5: "xxx" },
+    { c1: "xxx", c2: "xxx", c3: "xxx", c4: "xxx", c5: "xxx" }
   ];
 
   // ทำการผูกข้อมูลเพื่อนำไปแสดง
@@ -32,17 +32,22 @@ class Stock extends Component {
 
     // ใช้ condition if เพื่อที่จะตรวจเช็คว่า โปรแกรมมีค่าว่างหรือไม่(์Null) แต่ถ้าไม่เป็นค้าว่างก็ไม่ต้อง Return อะไรออกมานั้นเอง
     if (result != null) {
-      return result.map((item) => (
+      return result.map(item => (
         <tr>
           <td>{item.id}</td>
           <td>{item.name}</td>
           <td>{item.image}</td>
           <td>{item.price}</td>
           <td>{item.stock}</td>
+          <td>xxx</td>
         </tr>
       ));
     }
   };
+
+  onChange() {
+    
+  }
 
   render() {
     return (
@@ -51,66 +56,131 @@ class Stock extends Component {
         <section className="content-header">
           <h1>
             Stock
-            <small>advanced tables</small>
+            <small>Report</small>
           </h1>
           <ol className="breadcrumb">
             <li>
-              <a href="#">
+              <a href="#/">
                 <i className="fa fa-dashboard" /> Home
               </a>
             </li>
             <li>
-              <a href="#">Tables</a>
+              <a href="#/">Stock</a>
             </li>
-            <li className="active">Data tables</li>
           </ol>
         </section>
         {/* Main content */}
         <section className="content">
           <div className="row">
             <div className="col-xs-12">
-              <div className="box">
-                <div className="box-header">
-                  <h3 className="box-title">Hover Data Table</h3>
+              <div className="row">
+                <div className="col-md-4 col-sm-6 col-xs-12">
+                  <div className="info-box">
+                    <span className="info-box-icon">
+                      <img
+                        src={`${process.env.PUBLIC_URL}/images/ic_product.png`}
+                        className="logo"
+                      />
+                    </span>
+                    <div className="info-box-content">
+                      <p className="p_custom">Products</p>
+                      <h2>12</h2>
+                    </div>
+                  </div>
                 </div>
-                {/* /.box-header */}
+                <div className="col-md-4 col-sm-6 col-xs-12">
+                  <div className="info-box">
+                    <span className="info-box-icon">
+                      <img
+                        src={`${process.env.PUBLIC_URL}/images/ic_new.png`}
+                        className="logo"
+                      />
+                    </span>
+                    <div className="info-box-content">
+                      <p className="p_custom">Defect</p>
+                      <h2>0</h2>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-4 col-sm-6 col-xs-12">
+                  <div className="info-box">
+                    <span className="info-box-icon">
+                      <img
+                        src={`${process.env.PUBLIC_URL}/images/ic_out_of_stock.png`}
+                        className="logo"
+                      />
+                    </span>
+                    <div className="info-box-content">
+                      <p className="p_custom">SoldOut</p>
+                      <h2>12</h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="box">
                 <div className="box-body">
+                  <div className="row" style={{ marginBottom: 40 }}>
+                    <div className="col-xs-6">
+                      <input
+                        onChange={this.onChange}
+                        type="search"
+                        className="form-control input-lg"
+                        placeholder="Enter search keyword"
+                        style={{ borderRadius: 10 }}
+                      />
+                    </div>
+                    <div className="col-xs-6 text-right">
+                      <Link
+                        to="/stock-create"
+                        style={{ float: "right", margin: 0, width: 100 }}
+                        className="btn btn-success btn-lg"
+                      >
+                        เพิ่ม
+                      </Link>
+                    </div>
+                  </div>
+
                   <table
-                    id="example2"
-                    className="table table-bordered table-hover"
+                    id="stock_table"
+                    className="table table-bordered table-striped table-hover"
+                    style={{ height: 300, maxHeight: 300 }}
                   >
                     <thead>
                       <tr>
-                        <th>Rendering engine</th>
-                        <th>Browser</th>
-                        <th>Platform(s)</th>
-                        <th>Engine version</th>
-                        <th>CSS grade</th>
+                        <th style={{ width: "7%", textAlign: "center" }}>
+                          CREATED
+                        </th>
+                        <th style={{ width: "50%" }}>NAME</th>
+                        <th style={{ width: "9%" }}>PRICE</th>
+                        <th style={{ width: "9%" }}>STOCK</th>
+                        <th style={{ width: "4%" }}>ID</th>
+                        <th style={{ width: "14%", textAlign: "center" }}>
+                          ACTION
+                        </th>
                       </tr>
                     </thead>
-                    {/* tbody */}
                     <tbody>{this.createRows()}</tbody>
                   </table>
                 </div>
+                {/* /.box-body */}
               </div>
             </div>
             {/* /.col */}
           </div>
           {/* /.row */}
         </section>
-        {/* /.content */}
       </div>
     );
   }
 }
 
 const mapStateToProps = ({ stockReducer }) => ({
-  stockReducer,
+  stockReducer
 });
 
 const mapDispatchToProps = {
   // spreading
-  ...actions,
+  ...actions
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Stock);
